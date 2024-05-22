ade71f9e9b9d8a0514547e4caf4223a45821272c
juanf28 <chavarriajuanfelipe00@gmail.com>
2024-05-21 23:52:33 -0500

    Grafica completa

--- src/algorithm/V_3_Sequential_Block.py
+++ src/algorithm/V_3_Sequential_Block.py
@@ -3,21 +3,15 @@ class V_3_Sequential_Block:
     def alg_V_3_Sequential_Block(matrizA, matrizB):
         size1 = len(matrizA)
         size2 = len(matrizB)
-        if size1 == 0 or size2 == 0 or len(matrizA[0]) != size2:
-            raise ValueError("Dimensiones de matrices incompatibles para la multiplicación.")
-
-        block_size = 64  # Tamaño del bloque, ajústalo según sea necesario
         matrizRes = [[0 for _ in range(size1)] for _ in range(size1)]
 
-        for i1 in range(0, size1, block_size):
-            for j1 in range(0, size1, block_size):
-                for k1 in range(0, size2, block_size):
-                    for i in range(i1, min(i1 + block_size, size1)):
-                        for j in range(j1, min(j1 + block_size, size1)):
-                            sum = 0
-                            for k in range(k1, min(k1 + block_size, size2)):
-                                sum += matrizA[i][k] * matrizB[k][j]
-                            matrizRes[i][j] += sum
+        for i1 in range(0, size1, size2):
+            for j1 in range(0, size1, size2):
+                for k1 in range(0, size1, size2):
+                    for i in range(i1, min(i1 + size2, size1)):
+                        for j in range(j1, min(j1 + size2, size1)):
+                            for k in range(k1, min(k1 + size2, size1)):
+                                matrizRes[k][i] += matrizA[k][j] * matrizB[j][i]
 
         return matrizRes
 
--- src/algorithm/V_4_Parallel_Block.py
+++ src/algorithm/V_4_Parallel_Block.py
@@ -3,23 +3,27 @@ from multiprocessing import Pool
 
 class V_4_Parallel_Block:
     @staticmethod
+    def update_cell(i1, size1, size2, matrizA, matrizB, matrizC):
+        for j1 in range(0, size1, size2):
+            for k1 in range(0, size1, size2):
+                for i in range(i1, min(i1 + size2, size1)):
+                    for j in range(j1, min(j1 + size2, size1)):
+                        for k in range(k1, min(k1 + size2, size1)):
+                            matrizC[k, i] += matrizA[k][j] * matrizB[j][i]  # Convertir a acceso de lista
+
+    @staticmethod
     def alg_V_4_ParallelBlockTres(matrizA, matrizB, size1, size2):
         matrizC = np.zeros((size1, size1))
 
-        def update_cell(i1):
-            for j1 in range(0, size1, size2):
-                for k1 in range(0, size1, size2):
-                    for i in range(i1, min(i1 + size2, size1)):
-                        for j in range(j1, min(j1 + size2, size1)):
-                            for k in range(k1, min(k1 + size2, size1)):
-                                matrizC[k, i] += matrizA[k, j] * matrizB[j, i]
-
         with Pool() as pool:
-            pool.map(update_cell, range(0, size1, size2))
+            arguments = [(i1, size1, size2, matrizA, matrizB, matrizC) for i1 in range(0, size1, size2)]
+            pool.starmap(V_4_Parallel_Block.update_cell, arguments)
 
         return matrizC
 
     @staticmethod
     def multiply(matrizA, matrizB):
-        N, P = matrizA.shape[0], matrizB.shape[0]
-        return V_4_Parallel_Block.alg_V_4_ParallelBlockTres(matrizA, matrizB, N, P)
+        N, P = len(matrizA), len(matrizB[0])  # Corregir longitud de listas
+        matrizA_np = np.array(matrizA)  # Convertir lista a matriz NumPy
+        matrizB_np = np.array(matrizB)  # Convertir lista a matriz NumPy
+        return V_4_Parallel_Block.alg_V_4_ParallelBlockTres(matrizA_np, matrizB_np,N,P)
